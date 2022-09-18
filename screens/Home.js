9728236461a7879806459038183d0914d3b2d229
asumanwebdev <asumanwebdev@gmail.com>
2022-09-18 13:30:50 +0300

    add poppins font successfully

--- screens/Home.js
+++ screens/Home.js
@@ -2,14 +2,13 @@ import { View, SafeAreaView, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import tw from 'twrnc';
 import AppLoading from 'expo-app-loading';
-//import { useFonts } from 'expo-font';
-import { useFonts, Poppins_300Light, Poppins_700Bold } from '@expo-google-fonts/inter';
+import { useFonts, Poppins_300Light, Poppins_700Bold } from '@expo-google-fonts/poppins';
 
 const Home = () => {
-  // const [fontLoaded] = useFonts({
-  //   Poppins_300Light,
-  //   Poppins_700Bold,
-  // });
+  const [fontLoaded] = useFonts({
+    Poppins_300Light,
+    Poppins_700Bold,
+  });
   
   // if (!fontLoaded ) {
   //   return <AppLoading />;
@@ -17,7 +16,7 @@ const Home = () => {
 
   return (
     <SafeAreaView style={tw`bg-[#0f172a]`}>
-      <Text style={styles.title}>bmi calculator</Text>
+      <Text style={tw`text-2xl font-extrabold m-5 android:mt-12 uppercase ios:mt-20 text-[#fff]`}>bmi calculator</Text>
     </SafeAreaView>
   )
 }
@@ -25,9 +24,12 @@ const Home = () => {
 const styles = StyleSheet.create({
   title: {
     fontFamily: 'Poppins_700Bold',
+    fontSize: 24,
+    color: '#fff',
   }
 });
 
 //{tw`${styles.title} text-2xl font-extrabold m-5 android:mt-12 uppercase ios:mt-20 text-[#fff]`}
+
 
 export default Home

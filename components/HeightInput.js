36994c68bb38c8e140438c5077f9a56cdc2f6841
asumanwebdev <asumanwebdev@gmail.com>
2022-10-08 22:52:15 +0300

    add height input

--- components/HeightInput.js
+++ components/HeightInput.js
@@ -6,9 +6,9 @@ import AppLoading from 'expo-app-loading';
 import { styles } from './Inputs';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
-const WeightInput = () => {
- const [weightInputValue, setWeightInputValue] = useState('');
- const [unit, setUnit] = useState('Kgs');
+const HeightInput = () => {
+ const [heightInputValue, setHeightInputValue] = useState('');
+ const [unit, setUnit] = useState('m');
  const [displayUnits, setDisplayUnits] = useState(false);
 
  const [fontLoaded] = useFonts({
@@ -19,47 +19,47 @@ const WeightInput = () => {
   return <AppLoading />;
  }
 
- const incrementAge = () => {
-  weightInputValue === '' ? setWeightInputValue(1) : setWeightInputValue(parseInt(weightInputValue) + 1)
+ const incrementHeight = () => {
+  heightInputValue === '' ? setHeightInputValue(1) : setHeightInputValue(parseInt(heightInputValue) + 1)
  };
 
- const decrementAge = () => {
-  weightInputValue > 0 ? setWeightInputValue(weightInputValue - 1) : setWeightInputValue(0)
+ const decrementHeight = () => {
+  heightInputValue > 0 ? setHeightInputValue(heightInputValue - 1) : setHeightInputValue(0)
  };
 
- const setKgs = () => {
+ const setM = () => {
   setDisplayUnits(false);
-  setUnit('Kgs');
+  setUnit('m');
  };
- 
- const setIbs = () => {
+
+ const setFt = () => {
   setDisplayUnits(false);
-  setUnit('Ibs');
+  setUnit('ft');
  };
 
  //console.log(weightInputValue);
 
  return (
   <View style={tw`mt-6`}>
-  {
-   displayUnits &&  (<View style={tw`bg-white absolute w-[50%] right-1 bg-[#475569] h-16 z-3 rounded`}>
-    <Text style={tw`${unit === 'Kgs' && 'bg-[#0284c7]'} px-2 py-1`} onPress={setKgs}>Kgs</Text>
-    <Text style={tw`${unit === 'Ibs' && 'bg-[#0284c7]'} px-2 py-1`} onPress={setIbs}>Ibs</Text>
-   </View>)
-  } 
+   {
+    displayUnits && (<View style={tw`bg-white absolute w-[50%] right-1 bg-[#475569] h-16 z-3 rounded`}>
+     <Text style={tw`${unit === 'm' && 'bg-[#0284c7]'} px-2 py-1`} onPress={setM}>Kgs</Text>
+     <Text style={tw`${unit === 'ft' && 'bg-[#0284c7]'} px-2 py-1`} onPress={setFt}>Ibs</Text>
+    </View>)
+   }
    <Text style={styles.title}>
     weight
    </Text>
    <View style={tw`flex flex-row`}>
     <View style={tw`w-[65%]`}>
-     <TouchableOpacity style={tw`absolute right-2 z-2 top-4`} onPress={incrementAge}>
+     <TouchableOpacity style={tw`absolute right-2 z-2 top-4`} onPress={incrementHeight}>
       <FontAwesome5 name='plus-circle' size={38} />
      </TouchableOpacity>
      <TextInput style={tw`bg-white rounded-sm h-14 mt-2 text-xl font-semibold text-[#0f172a] pl-2 w-full text-center`}
       placeholder='e.g 77'
-      onChangeText={text => setWeightInputValue(text)} keyboardType="number-pad"
-      mode="outlined" >{weightInputValue}</TextInput>
-     <TouchableOpacity style={tw`absolute top-4 left-2`} onPress={decrementAge}>
+      onChangeText={text => setHeightInputValue(text)} keyboardType="number-pad"
+      mode="outlined" >{heightInputValue}</TextInput>
+     <TouchableOpacity style={tw`absolute top-4 left-2`} onPress={decrementHeight}>
       <FontAwesome5 name='minus-circle' size={38} />
      </TouchableOpacity>
     </View>
@@ -74,4 +74,4 @@ const WeightInput = () => {
  )
 }
 
-export default WeightInput;
+export default HeightInput;
